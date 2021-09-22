8f4873a29a1992af3a3f057d5a017f99c8f6b56d
Mehmood <m.hassan.rahi@gmail.com>
2021-09-22 14:49:58 +0500

    * Updated Header Button

--- src/components/Headers/HeaderButton.js
+++ src/components/Headers/HeaderButton.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
-const HeaderButton = ({ title }) => {
+const HeaderButton = (props) => {
     return (
-        <View>
-            <Text>{title}</Text>
-        </View>
+        <TouchableOpacity
+            disabled={props.disabled}
+            style={[styles.button, props.style]}
+            onPress={() => props.onPress(props.title)}
+        >
+            <Text style={[styles.title, props.titleStyle]}>{props.title}</Text>
+        </TouchableOpacity>
     );
 };
 
 export default HeaderButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "black",
+        borderWidth: 1,
+        borderRadius: wp(10),
+        width: wp(30),
+        height: hp(5),
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: wp(1),
+    },
+
+    title: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: hp(2),
+    },
+});
