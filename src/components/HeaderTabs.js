4959912cecbc3edc9af11e538a01f3a581d93b02
Mehmood <m.hassan.rahi@gmail.com>
2021-09-22 11:41:12 +0500

    * Added HeaderTabs component

---
+++ src/components/HeaderTabs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const HeaderTabs = () => {
+    return (
+        <View>
+            <Text>Header Tabs</Text>
+        </View>
+    );
+};
+
+export default HeaderTabs;
+
+const styles = StyleSheet.create({});
