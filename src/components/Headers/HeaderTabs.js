5adf5a3f7633f5fabbf82d9f9798ea43cc4d5a0a
Mehmood <m.hassan.rahi@gmail.com>
2021-09-22 12:45:34 +0500

    * Added HeaderTabs component

---
+++ src/components/Headers/HeaderTabs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import HeaderButton from "./HeaderButton";
+
+const HeaderTabs = () => {
+    return (
+        <View>
+            <HeaderButton title="Delivery" />
+            <HeaderButton title="Delivery" />
+        </View>
+    );
+};
+
+export default HeaderTabs;
+
+const styles = StyleSheet.create({});
