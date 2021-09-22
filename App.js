c9d927bc7d124e49607a022865bfe7d945847c8d
Mehmood <m.hassan.rahi@gmail.com>
2021-09-22 20:49:43 +0500

    * Updated App background Color

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { SafeAreaView, StyleSheet, Platform } from "react-native";
 import Constants from "expo-constants";
 import Home from "./src/screens/Home";
+import { COLORS } from "./src/constants";
 
 export default function App() {
     return (
@@ -15,5 +16,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+        backgroundColor: COLORS.background,
     },
 });
